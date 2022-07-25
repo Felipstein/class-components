bb509c6da43f1a5ada69889d1c653721a09ee792
Felipstein <luisfelipe-oliveira@outlook.com.br>
2022-07-24 23:59:52 -0300

    PostsList refactored to class

--- src/components/PostsList/index.js
+++ src/components/PostsList/index.js
@@ -3,18 +3,21 @@ import React from 'react';
 import { Container } from './styles';
 import Post from './Post';
 
-import posts from './posts';
+import posts from './posts'
+export default class PostsList extends React.Component {
 
-export default function PostsList() {
-  return (
-    <Container>
-      {posts.map((post) => (
-        <Post
-          key={post.id}
-          title={post.title}
-          description={post.description}
-        />
-      ))}
-    </Container>
-  );
+  render() {
+    return (
+      <Container>
+        {posts.map((post) => (
+          <Post
+            key={post.id}
+            title={post.title}
+            description={post.description}
+          />
+        ))}
+      </Container>
+    );
+  }
+
 }
