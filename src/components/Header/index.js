cee2e2f74085cb51ecee300f75608c618f52d207
Felipstein <luisfelipe-oliveira@outlook.com.br>
2022-07-25 22:42:35 -0300

    Comments of Functional Component in Header deleted

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -21,14 +21,3 @@ export default class Header extends Component {
 
 }
 
-// export default function Header({ onToggleTheme, onSetPurpleTheme, selectedTheme }) {
-//   return (
-//     <Container>
-//       <h1>JStack's Blog</h1>
-//       <Button onClick={onToggleTheme}>
-//         {selectedTheme === 'dark' ? '🌞' : '🌚'}
-//       </Button>
-//       <Button onClick={onSetPurpleTheme}>🟣</Button>
-//     </Container>
-//   );
-// }
