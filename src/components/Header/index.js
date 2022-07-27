d598bb652d14163ec509ede5b55c3550180367da
Felipstein <luisfelipe-oliveira@outlook.com.br>
2022-07-26 22:33:41 -0300

    refactored Context API in Header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,28 +1,28 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+
+import { ThemeContext } from '../../contexts/ThemeContext';
 
 import { Container } from './styles';
 import Button from '../Button';
 
 export default class Header extends Component {
 
-  static propTypes = {
-    selectedTheme: PropTypes.string.isRequired,
-    onToggleTheme: PropTypes.func.isRequired,
-    onSetPurpleTheme: PropTypes.func.isRequired,
-  }
-
   render() {
-    const { onToggleTheme, onSetPurpleTheme, selectedTheme } = this.props;
 
     return (
-    <Container>
-      <h1>JStack's Blog</h1>
-      <Button onClick={onToggleTheme}>
-        {selectedTheme === 'dark' ? '🌞' : '🌚'}
-      </Button>
-      <Button onClick={onSetPurpleTheme}>🟣</Button>
-    </Container>
+      <ThemeContext.Consumer>
+        {
+          ({ theme, handleToggleTheme, handleSetPurpleTheme }) => (
+            <Container>
+              <h1>JStack's Blog</h1>
+              <Button onClick={handleToggleTheme}>
+                {theme === 'dark' ? '🌞' : '🌚'}
+              </Button>
+              <Button onClick={handleSetPurpleTheme}>🟣</Button>
+            </Container>
+          )
+        }
+      </ThemeContext.Consumer> 
     );
   }
 
