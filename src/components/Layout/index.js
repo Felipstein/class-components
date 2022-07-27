1f39a9a8cc2a811420179a7890c4626933bdad0f
Felipstein <luisfelipe-oliveira@outlook.com.br>
2022-07-26 22:35:45 -0300

    simplified Layout component

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,28 +1,15 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 import Header from '../Header';
 import PostsList from '../PostsList';
 import Footer from '../Footer';
 
-export default function Layout({ onToggleTheme, onSetPurpleTheme, selectedTheme }) {
+export default function Layout() {
   return (
     <>
-      <Header
-        onToggleTheme={onToggleTheme}
-        onSetPurpleTheme={onSetPurpleTheme}
-        selectedTheme={selectedTheme}
-        />
+      <Header />
       <PostsList />
-      <Footer
-        onToggleTheme={onToggleTheme}
-        onSetPurpleTheme={onSetPurpleTheme}
-        selectedTheme={selectedTheme}
-      />
+      <Footer />
     </>
   );
-}
-
-Layout.propTypes = {
-  selectedTheme: PropTypes.string.isRequired,
 }
