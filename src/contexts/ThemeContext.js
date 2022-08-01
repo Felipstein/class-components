6891b65b7d2f86dc30b8cee62f94c9d56d294cb1
Felipstein <luisfelipe-oliveira@outlook.com.br>
2022-07-31 22:50:05 -0300

    added theme in localStorage

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -4,18 +4,33 @@ export const ThemeContext = createContext();
 
 export class ThemeProvider extends React.Component {
 
-  state = {
-    theme: 'dark',
+  constructor(props) {
+    super(props);
+
+    let theme = 'dark';
+    try {
+      theme = JSON.parse(localStorage.getItem('theme'));
+    } catch(err) {
+      console.log(err);
+    }
+
+    this.state = {
+      theme,
+    }
   }
 
   handleToggleTheme = () => {
     this.setState(prevState => ({
       theme: prevState.theme === 'dark' ? 'light' : 'dark'
-    }))
+    }), this.handleLocalStorageTheme);
   }
 
   handleSetPurpleTheme = () => {
-    this.setState({ theme: 'purple' });
+    this.setState({ theme: 'purple' }, this.handleLocalStorageTheme);
+  }
+
+  handleLocalStorageTheme = () => {
+    localStorage.setItem('theme', JSON.stringify(this.state.theme));
   }
 
   render() {
