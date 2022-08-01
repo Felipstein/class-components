85f4cfc22f112c2f8f88989b5f1350a071e58e8f
Felipstein <luisfelipe-oliveira@outlook.com.br>
2022-07-31 22:29:45 -0300

    testing lifecycles of class components and refactored Layout component

--- src/App.js
+++ src/App.js
@@ -9,10 +9,55 @@ import themes from './styles/themes';
 
 class App extends React.Component {
 
+  state = {
+    changed: false,
+  }
+
+  componentDidMount() {
+    console.debug('componentDidMount executed')
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.debug({
+      currentState: this.state,
+      currentProps: this.props,
+      prevState,
+      prevProps,
+    });
+  }
+
+  componentDidCatch(error, info) {
+
+    console.debug({ error, info });
+
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+
+    console.debug({
+      currentState: this.state,
+      currentProps: this.props,
+      nextState,
+      nextProps,
+    });
+
+    return true;
+  }
+
+  componentWillUnmount() {
+    console.debug('Tchau tchau!!!')
+  }
+
   render() {
+    console.debug('rendered');
 
     return (
       <ThemeProvider>
+
+        <button onClick={() => this.setState({ changed: true })}>
+          Change state
+        </button>
+
         <ThemeContext.Consumer>
           {({ theme }) => (
             <StyledThemeProvider theme={themes[theme] || themes.dark}>
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -4,12 +4,29 @@ import Header from '../Header';
 import PostsList from '../PostsList';
 import Footer from '../Footer';
 
-export default function Layout() {
-  return (
-    <>
-      <Header />
-      <PostsList />
-      <Footer />
-    </>
-  );
+export default class Layout extends React.Component {
+
+  componentDidMount() {
+    document.addEventListener('scroll', this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    console.debug('Componente vai desmontar...');
+    document.removeEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll = () => {
+    console.debug('scrolled...');
+  }
+
+  render() {
+    return (
+      <>
+        <Header />
+        <PostsList />
+        <Footer />
+      </>
+    );
+  }
+
 }
